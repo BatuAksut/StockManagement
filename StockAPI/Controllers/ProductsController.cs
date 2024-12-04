64b8c64d96691e8e12e2724febdba1b1984d8327
BatuAksut <batuhanaksut@hotmail.com>
2024-12-04 22:30:13 +0300

    API added

---
+++ StockAPI/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockManagement.Data;
+using StockManagement.Models;
+
+namespace StockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StockDbContext _db;
+
+        public ProductsController(StockDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = _db.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = _db.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            if (product == null)
+                return BadRequest();
+
+            _db.Products.Add(product);
+            _db.SaveChanges();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(Product product)
+        {
+            var updatedEntity = _db.Entry(product);
+            updatedEntity.State = EntityState.Modified;
+            _db.SaveChanges();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _db.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+                return NotFound();
+
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return NoContent();
+        }
+    }
+
+}
