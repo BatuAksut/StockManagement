4077faea55e2530175ab013a1ce37ba6fbe11c69
BatuAksut <batuhanaksut@hotmail.com>
2024-12-07 23:32:29 +0300

    api implementation error fixed

--- StockAPI/Controllers/ProductsController.cs
+++ StockAPI/Controllers/ProductsController.cs
@@ -46,13 +46,31 @@ namespace StockAPI.Controllers
             return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
         }
         [HttpPut("{id}")]
-        public IActionResult UpdateProduct(Product product)
+        public IActionResult UpdateProduct(int id, Product product)
         {
-            var updatedEntity = _db.Entry(product);
-            updatedEntity.State = EntityState.Modified;
+   
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+        
+            var existingProduct = _db.Products.Find(id);
+
+           
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            existingProduct.ProductName = product.ProductName;
+            existingProduct.UnitsInStock = product.UnitsInStock;
+            existingProduct.CategoryId = product.CategoryId;
             _db.SaveChanges();
+
             return NoContent();
         }
+
         [HttpDelete("{id}")]
         public IActionResult DeleteProduct(int id)
         {
